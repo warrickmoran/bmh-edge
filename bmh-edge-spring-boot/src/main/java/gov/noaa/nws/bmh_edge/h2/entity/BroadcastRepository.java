07ed4a2e173cb090cc37dd328daba784e3f19c42
warrickmoran <Warrick.Moran@noaa.gov>
2018-04-04 18:57:41 +0000

    add jparepository comments and examples to BroadcastRepository.java

--- bmh-edge-spring-boot/src/main/java/gov/noaa/nws/bmh_edge/h2/entity/BroadcastRepository.java
+++ bmh-edge-spring-boot/src/main/java/gov/noaa/nws/bmh_edge/h2/entity/BroadcastRepository.java
@@ -3,6 +3,8 @@ package gov.noaa.nws.bmh_edge.h2.entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+// persistence technology-specific abstractions 
+// https://docs.spring.io/spring-data/jpa/docs/current/reference/html/
 @Repository
 public interface BroadcastRepository extends JpaRepository<BroadcastMessage, Long>{
 
