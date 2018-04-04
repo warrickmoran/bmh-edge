dbe1abfe04c80854092df0f445b60d73387d3a57
warrickmoran <Warrick.Moran@noaa.gov>
2018-04-04 18:59:48 +0000

    additional comments for hibernate/h2 under spring boot

--- bmh-edge-spring-boot/src/main/java/gov/noaa/nws/bmh_edge/h2/entity/BroadcastRepository.java
+++ bmh-edge-spring-boot/src/main/java/gov/noaa/nws/bmh_edge/h2/entity/BroadcastRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 // persistence technology-specific abstractions 
 // https://docs.spring.io/spring-data/jpa/docs/current/reference/html/
+// https://www.concretepage.com/spring-5/spring-data-crudrepository-example
 @Repository
 public interface BroadcastRepository extends JpaRepository<BroadcastMessage, Long>{
 
